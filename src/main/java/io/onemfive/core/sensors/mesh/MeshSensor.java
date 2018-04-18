8ffd906473b54d20a6b1666de8d1723d495d9c95
Brian Taylor <objectorange@protonmail.com>
2018-04-18 13:51:45 -0400

    print outs for mesh sensor

--- src/main/java/io/onemfive/core/sensors/mesh/MeshSensor.java
+++ src/main/java/io/onemfive/core/sensors/mesh/MeshSensor.java
@@ -13,7 +13,9 @@ public class MeshSensor implements Sensor {
 
     @Override
     public boolean start(Properties properties) {
+        System.out.println("Starting MeshSensor...");
 
+        System.out.println("MeshSensor started.");
         return true;
     }
 
@@ -34,7 +36,9 @@ public class MeshSensor implements Sensor {
 
     @Override
     public boolean shutdown() {
+        System.out.println("Shutting down MeshSensor...");
 
+        System.out.println("MeshSensor shutdown.");
         return true;
     }
 
