79da77f3396528f6b680bf7de4e31df375cab824
Brian Taylor <objectorange@protonmail.com>
2018-04-18 20:10:56 -0400

    need a persistent queue long-term to ensure messages are not lost once received by the service bus.

---
+++ src/main/java/io/onemfive/core/bus/PersistentMessageQueue.java
@@ -0,0 +1,50 @@
+package io.onemfive.core.bus;
+
+import java.util.Collection;
+import java.util.concurrent.ArrayBlockingQueue;
+
+/**
+ * Provides persistence to ArrayBlockingQueue
+ *
+ * TODO: Implement and place in Message Channel
+ *
+ * @author objectorange
+ */
+public class PersistentMessageQueue<E> extends ArrayBlockingQueue<E> {
+
+    public PersistentMessageQueue(int capacity) {
+        super(capacity);
+    }
+
+    public PersistentMessageQueue(int capacity, boolean fair) {
+        super(capacity, fair);
+    }
+
+    public PersistentMessageQueue(int capacity, boolean fair, Collection<? extends E> c) {
+        super(capacity, fair, c);
+    }
+
+    @Override
+    public boolean add(E e) {
+        boolean success = super.add(e);
+        if(success) {
+            // persist
+
+        }
+        return success;
+    }
+
+    @Override
+    public E take() throws InterruptedException {
+        E obj = super.take();
+
+        return obj;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        boolean success = super.remove(o);
+
+        return success;
+    }
+}
