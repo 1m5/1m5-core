32352dd5514c0c87391a810fbee7231847694c96
ObjectOrange <objectorange@protonmail.com>
2018-07-03 23:40:03 -0400

    provide an interface for other code to listen in on client status updates

---
+++ src/main/java/io/onemfive/core/client/ClientStatusListener.java
@@ -0,0 +1,10 @@
+package io.onemfive.core.client;
+
+/**
+ * TODO: Add Description
+ *
+ * @author objectorange
+ */
+public interface ClientStatusListener {
+    void clientStatusChanged(ClientAppManager.Status clientStatus);
+}
