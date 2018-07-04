af4403a7416c3665f6b3f7deab3a4809ccfa78ec
ObjectOrange <objectorange@protonmail.com>
2018-07-03 23:42:22 -0400

    clean up cleaners

---
+++ src/main/java/io/onemfive/core/sensors/cleaner/corporate/CorporateCleaner.java
@@ -0,0 +1,11 @@
+package io.onemfive.core.sensors.cleaner.corporate;
+
+import io.onemfive.core.sensors.cleaner.Cleaner;
+
+/**
+ * TODO: Add Description
+ *
+ * @author objectorange
+ */
+public class CorporateCleaner implements Cleaner {
+}
