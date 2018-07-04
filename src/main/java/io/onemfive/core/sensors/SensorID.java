7862526a55dbad29b0cd0b86a26c2abb67fa991d
ObjectOrange <objectorange@protonmail.com>
2018-07-03 23:26:09 -0400

    Use enum for sensor identity.

---
+++ src/main/java/io/onemfive/core/sensors/SensorID.java
@@ -0,0 +1,18 @@
+package io.onemfive.core.sensors;
+
+/**
+ * TODO: Add Description
+ *
+ * @author objectorange
+ */
+public enum SensorID {
+    BLUETOOTH, BLUETOOTHLE,
+    CLEARNET,
+    HAM,
+    I2P, I2PBOTE,
+    MESH,
+    NEARBY,
+    NFC,
+    TOR,
+    WIFIAWARE, WIFIDIRECT, WIFIHALOW, WIFINTERNET
+}
