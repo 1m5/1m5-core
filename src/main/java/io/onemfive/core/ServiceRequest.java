c86f0ed34b5d6c0949873aaf41077915ff6276bf
objectorange <objectorange@protonmail.com>
2018-08-03 08:50:36 -0400

    add json serializable interface

--- src/main/java/io/onemfive/core/ServiceRequest.java
+++ src/main/java/io/onemfive/core/ServiceRequest.java
@@ -1,6 +1,25 @@
 package io.onemfive.core;
 
-public abstract class ServiceRequest {
+import io.onemfive.data.JSONSerializable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class ServiceRequest implements JSONSerializable {
     public String errorMessage;
     public Exception exception;
+
+    public Map<String,Object> toMap() {
+        Map<String,Object> m = new HashMap<>();
+        m.put("errorMessage",errorMessage);
+        m.put("exception",exception.getLocalizedMessage());
+        return m;
+    }
+
+    public void fromMap(Map<String,Object> m) {
+        if(m.get("errorMessage")!=null) this.errorMessage = (String)m.get("errorMessage");
+        if(m.get("exception")!=null) {
+            exception = new Exception((String)m.get("exception"));
+        }
+    }
 }
