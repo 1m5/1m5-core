bde2069646c7180ee16a300367390a1b82245aa9
objectorange <objectorange@protonmail.com>
2018-09-08 00:24:23 -0400

    change description

--- src/main/java/io/onemfive/core/sensors/tor/TorSensor.java
+++ src/main/java/io/onemfive/core/sensors/tor/TorSensor.java
@@ -13,11 +13,7 @@ import java.util.Properties;
 import java.util.logging.Logger;
 
 /**
- * Provides an API for Tor Router.
- * By default, looks for a running Tor instance.
- * If discovered and is configured appropriately, will use it.
- * If discovered and is not configured appropriately, will launch new configured instance.
- * If not found to be installed, will send a message to end user that they need to install Tor (Orbot on Android).
+ * Orbot embedded.
  *
  * @author objectorange
  */
