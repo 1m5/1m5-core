60ae61acafa417803a9838811092227dfa1e055c
objectorange <objectorange@protonmail.com>
2018-11-28 14:04:47 -0500

    provide logic on how keys are returned; specify what's required and what's defaulted; provide a returned PublicKey

--- src/main/java/io/onemfive/core/keyring/LoadKeyRingsRequest.java
+++ src/main/java/io/onemfive/core/keyring/LoadKeyRingsRequest.java
@@ -1,14 +1,30 @@
 package io.onemfive.core.keyring;
 
 import io.onemfive.core.ServiceRequest;
+import io.onemfive.data.PublicKey;
 
 public class LoadKeyRingsRequest extends ServiceRequest {
-    public static int ALIAS_REQUIRED = 1;
-    public static int PASSPHRASE_REQUIRED = 2;
+    public static int KEY_RING_ALIAS_REQUIRED = 1;
+    public static int KEY_RING_PASSPHRASE_REQUIRED = 2;
     public static int AUTOGENERATE_REMOVE_OLD_KEYS_CONFLICT = 3;
+    public static int SKR_LOCATION_NOT_PROVIDED = 4;
+    public static int PKR_LOCATION_NOT_PROVIDED = 5;
 
-    public String alias;
-    public char[] passphrase;
+    // Required
+    public String keyRingAlias;
+    // Required
+    public char[] keyRingPassphrase;
+    // Required
+    public String secretKeyRingCollectionFileLocation;
+    // Required
+    public String publicKeyRingCollectionFileLocation;
+
+    // If publicKeyFingerprint is provided, will use it to lookup up public key.
+    public String publicKeyFingerprint;
+    // Otherwise if publicKeyAlias is provided, will look up public key by that.
+    public String publicKeyAlias;
+    // Otherwise if master is true will return the master key, if false will return the signing key.
+    public boolean master = true; // default
     /**
      * hashStrength: a number between 0 and 0xff that controls the number of times to iterate the password
      * hash before use. More iterations are useful against offline attacks, as it takes more
@@ -19,9 +35,10 @@ public class LoadKeyRingsRequest extends ServiceRequest {
      * and so on till 0xf0, or about 1 million iterations. The maximum you can go to is 0xff,
      * or about 2 million iterations. These values are constants in the KeyRingService class as helpers.
      */
-    public int hashStrength = KeyRingService.PASSWORD_HASH_STRENGTH_64;
-    public String secretKeyRingCollectionFileLocation = "1m5.skr";
-    public String publicKeyRingCollectionFileLocation = "1m5.pkr";
-    public boolean autoGenerate = true;
-    public boolean removeOldKeys = false;
+    public int hashStrength = KeyRingService.PASSWORD_HASH_STRENGTH_64; // default
+    public boolean autoGenerate = true; // default
+    public boolean removeOldKeys = false; // default
+    // Response
+    public PublicKey publicKey;
+
 }
